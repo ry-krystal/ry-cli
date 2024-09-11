f480ca5521d0d764a39288cfba544426f90c7d3d
renyong <995151172@qq.com>
2024-09-11 18:08:39 +0800

    [commit]: 0.0.5

--- initAction.js
+++ initAction.js
@@ -67,6 +67,7 @@ const initAction = async (name, option) => {
   if (!option.ignore) {
     // 询问
     const answers = await inquirerInputs(messages);
+    console.log('answer', answers);
     await changePackageJson(name, answers)
   }
 
